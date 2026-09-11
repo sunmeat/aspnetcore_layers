f439a974171078e7c33db92ea558f7c5e8be649f
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2026-09-11 19:18:49 +0300

    Fix PlayerRepository to use db.Player instead of db.Players

--- Soccer.DAL/Repositories/PlayerRepository.cs
+++ Soccer.DAL/Repositories/PlayerRepository.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore; 
 using Soccer.DAL.Entities;           
 using Soccer.DAL.Interfaces;         
 using Soccer.DAL.EF;             
@@ -16,26 +16,26 @@ namespace Soccer.DAL.Repositories
 
         public async Task<IEnumerable<Player>> GetAll() // отримання всіх гравців
         {
-            return await db.Players.Include(o => o.Team).ToListAsync(); // завантажуємо гравців разом з пов'язаною командою (eager loading) та повертаємо список
+            return await db.Player.Include(o => o.Team).ToListAsync(); // завантажуємо гравців разом з пов'язаною командою (eager loading) та повертаємо список
         }
 
         public async Task<Player?> Get(int id) // асинхронний метод для отримання гравця за ідентифікатором
         {
-            var players = await db.Players.Include(o => o.Team).Where(a => a.Id == id).ToListAsync(); // фільтруємо та завантажуємо команду
+            var players = await db.Player.Include(o => o.Team).Where(a => a.Id == id).ToListAsync(); // фільтруємо та завантажуємо команду
             Player? player = players?.FirstOrDefault(); // беремо перший (або єдиний) елемент зі списку
             return player; // повертаємо знайденого гравця або null
         }
 
         public async Task<Player?> Get(string name) // асинхронний метод для отримання гравця за ім'ям
         {
-            var players = await db.Players.Where(a => a.Name == name).ToListAsync(); // фільтруємо за точним збігом імені без завантаження команди
+            var players = await db.Player.Where(a => a.Name == name).ToListAsync(); // фільтруємо за точним збігом імені без завантаження команди
             Player? player = players?.FirstOrDefault(); // беремо першого знайденого
             return player; // повертаємо гравця або null
         }
 
         public async Task Create(Player player) // асинхронний метод для створення нового гравця
         {
-            await db.Players.AddAsync(player); // додаємо сутність до dbset асинхронно (зміни зберігаються пізніше через savechanges)
+            await db.Player.AddAsync(player); // додаємо сутність до dbset асинхронно (зміни зберігаються пізніше через savechanges)
         }
 
         public void Update(Player player) // синхронний метод для оновлення гравця
@@ -45,9 +45,9 @@ namespace Soccer.DAL.Repositories
 
         public async Task Delete(int id) // асинхронний метод для видалення гравця за ідентифікатором
         {
-            Player? player = await db.Players.FindAsync(id); // ефективний пошук за первинним ключем асинхронно
+            Player? player = await db.Player.FindAsync(id); // ефективний пошук за первинним ключем асинхронно
             if (player != null) // перевірка на існування
-                db.Players.Remove(player); // видаляємо сутність з dbset (зміни застосовуються при savechanges)
+                db.Player.Remove(player); // видаляємо сутність з dbset (зміни застосовуються при savechanges)
         }
     }
 }
